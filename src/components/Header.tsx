3bb6f3b196f9dde8f17d43eaf7738c3b494b61ae
Antonio Hutabarat <antonio.hutabarat@live.com>
2023-03-19 01:13:59 -0500

    Update script

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,27 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 
 export const Header = () => {
-    const menus = [
-        {
-            title: "Home",
-            path: "/"
-        },
-        {
-            title: "Checkout",
-            path: "/checkout"
-        }
-    ]
+  const menus = [
+    {
+      title: "Home",
+      path: "/",
+    },
+    {
+      title: "Checkout",
+      path: "/checkout",
+    },
+  ];
 
-    return <div>
-        <ul className="bg-red-500 flex text-white">
-            {
-                menus.map((menu, index) =>
-                    <li key={index}><NavLink to={menu.path}>{menu.title}</NavLink></li>
-                )
-            }
-        </ul>
+  return (
+    <div className="bg-red-500 flex text-white">
+      Kantan Traveler
+      <ul className="flex">
+        {menus.map((menu, index) => (
+          <li key={index} className="p-8">
+            <NavLink to={menu.path}>{menu.title}</NavLink>
+          </li>
+        ))}
+      </ul>
     </div>
-}
+  );
+};
